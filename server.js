dcb376f3da236960c559d3b31d4022abecc811e7
Philppe <psalvail@assetscience.com>
2020-06-08 19:50:41 -0400

    m3-3-node-facespace exercise 1-1 completed

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const morgan = require('morgan');
-
-const { users } = require('./data/users');
-
+const express = require("express");
+const morgan = require("morgan");
+const { users } = require("./data/users");
 let currentUser = {};
 
 // declare the 404 function
@@ -12,17 +10,25 @@ const handleFourOhFour = (req, res) => {
   res.status(404).send("I couldn't find what you're looking for.");
 };
 
+// const q1 = (req, res) => {
+//   res.render("pages/homepage", { users });
+// };
+const handleHomepage = (req, res) => {
+  res.status(200).send("homepage");
+};
 // -----------------------------------------------------
 // server endpoints
+const PORT = process.env.PORT || 8000;
 express()
-  .use(morgan('dev'))
-  .use(express.static('public'))
+  .use(morgan("dev"))
+  .use(express.static("public"))
   .use(express.urlencoded({ extended: false }))
-  .set('view engine', 'ejs')
+  .set("view engine", "ejs")
 
   // endpoints
 
   // a catchall endpoint that will send the 404 message.
-  .get('*', handleFourOhFour)
+  .get("/", handleHomepage)
+  .get("*", handleFourOhFour)
 
-  .listen(8000, () => console.log('Listening on port 8000'));
+  .listen(8000, () => console.log(`Listening on port ${PORT} `));
