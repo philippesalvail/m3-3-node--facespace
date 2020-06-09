4d57b57ced2473f056aabca51b1d4814793c18cb
Philppe <psalvail@assetscience.com>
2020-06-09 18:50:37 -0400

    exercise 2.2 completed

--- server.js
+++ server.js
@@ -14,6 +14,28 @@ const q1 = (req, res) => {
   res.status(200).render("pages/homepage", { users });
 };
 
+const q2 = (req, res) => {
+  let user_id = req.params.user_id;
+  if (user_id < users[0]._id || user_id > users[users.length - 1]._id) {
+    res.status(404).send("User id does not exist.");
+  }
+  let currentUserFriends = [];
+  users.forEach(function (user) {
+    if (user_id == user._id) {
+      currentUser = user;
+    }
+  });
+  users.forEach(function (friend) {
+    currentUser.friends.forEach(function (currentUserFriend) {
+      if (currentUserFriend == friend._id) {
+        currentUserFriends.push(friend);
+      }
+    });
+  });
+
+  res.status(200).render("pages/profile", { currentUser, currentUserFriends });
+};
+
 // -----------------------------------------------------
 // server endpoints
 const PORT = process.env.PORT || 8000;
@@ -27,6 +49,7 @@ express()
 
   // a catchall endpoint that will send the 404 message.
   .get("/homepage", q1)
+  .get("/homepage/:user_id", q2)
   .get("*", handleFourOhFour)
 
   .listen(8000, () => console.log(`Listening on port ${PORT} `));
